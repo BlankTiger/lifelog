de08d0d6f65bd087200c4a5f386dd359968d870d
Maciej Urban <maciej-urban@outlook.com>
2022-11-02 08:51:21 +0100

    changed json schema

--- src-tauri/src/calendar_entry.rs
+++ src-tauri/src/calendar_entry.rs
@@ -1,20 +1,29 @@
-use chrono::{DateTime, Local};
-use serde::{Deserialize, Serialize};
 use crate::command_result::CommandResult;
-use std::fs;
+use chrono::{DateTime, Local, NaiveDate};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, fs};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PauseTime {
+    elapsed_time: i64,
+    actual_time: i64,
+    current_time: DateTime<Local>,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Entry {
     id: i64,
     title: String,
     desc: String,
-    start_time: DateTime<Local>,
-    end_time: DateTime<Local>,
+    start: DateTime<Local>,
+    end: DateTime<Local>,
+    pause_times: Vec<PauseTime>,
 }
 
 #[tauri::command]
-pub fn generate_from_file() -> CommandResult<Vec<Entry>> {
-    let file = fs::read_to_string("./entries.json").expect("New");
-    let entries: Vec<Entry> = serde_json::from_str(&file).expect("wow");
+pub fn generate_from_file() -> CommandResult<HashMap<NaiveDate, Vec<Entry>>> {
+    let file = fs::read_to_string("./entries.json").expect("Read a file");
+    let entries: HashMap<NaiveDate, Vec<Entry>> =
+        serde_json::from_str(&file).expect("Converted to json");
     Ok(entries)
 }
